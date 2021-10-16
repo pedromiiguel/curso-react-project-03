3aeff267503d7ce8828329f874ca44344bb22049
Pedro Miguel <pedromrap@gmail.com>
2021-10-16 10:35:38 -0300

    grid content component

---
+++ src/components/GridContent/index.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import P from 'prop-types';
+import * as Styled from './styles';
+import { SectionBackground } from '../SectionBackground';
+import { Heading } from '../Heading';
+import { Text } from '../Text';
+
+export const GridContent = ({ title, html, srcImage, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading uppercase size="big" colorDark={!background}>
+          {title}
+        </Heading>
+        <Styled.Html>
+          <Text>{html}</Text>
+        </Styled.Html>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridContent.propTypes = {
+  title: P.string.isRequired,
+  html: P.string.isRequired,
+  srcImage: P.string.isRequired,
+  background: P.bool,
+};
--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -19,8 +19,21 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   h1,h2,h3,h4,h5,h6 {
-    font-family: ${({ theme }) => theme.fonts.family.title}
+    font-family: ${({ theme }) => theme.fonts.family.title};
+    margin: ${({ theme }) => theme.spacings.large} 0;
 
+  }
 
+  p {
+    margin: ${({ theme }) => theme.spacings.medium} 0;
+  }
+
+  ul, ol {
+    margin: ${({ theme }) => theme.spacings.medium};
+    padding: ${({ theme }) => theme.spacings.medium};
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.secondaryColor}
   }
 `;
